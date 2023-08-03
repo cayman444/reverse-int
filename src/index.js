f6ea0aefec0e9ba50e5f92f286e4d08e7d96e9f4
cayman444 <artursavinX@yandex.ru>
2023-08-03 23:53:13 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let strN = n.toString()
+    let newStr = ''
+    for (let i = strN.length - 1; i >= 0; i--) {
+        if (strN[i] === '-') {
+            strN.slice(i)
+        } else {
+            newStr += strN[i]
+        }
+    }
+    return +newStr
 }
+
